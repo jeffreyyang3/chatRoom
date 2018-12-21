e63df4d085f8770972391b6fbdd3af2a18721a58
Jeffrey Yang <jeffreyyang@Jeffreys-MacBook-Air.local>
2018-12-20 18:00:58 -0800

    test beginnings

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+from . import views
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get('/chat/dankman558/')
+        self.assertEquals(response.status_code, 200)
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
-from .models import User
+
 
 def index(request):
     return render(request, 'chat/index.html', {})
